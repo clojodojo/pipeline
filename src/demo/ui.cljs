1f0a6ce4e298fd37e9e62fb1fddce57f177817e6
Rafal Dittwald <rafal.dittwald@gmail.com>
2021-11-03 01:02:21 +0000

    WIP allowing steps to reference each other via labels

--- src/demo/ui.cljs
+++ src/demo/ui.cljs
@@ -19,12 +19,16 @@
 
 (defonce state
   (r/atom
-    {:initial-input "{:x 1\n:y 2}"
-     :steps [{:code "(fn [i] (+ (:x i) (:y i)))"}]}))
-
-(defn change-initial-input! [e]
-  (swap! state assoc :initial-input
-    (.. e -target -value)))
+    {:steps [{:label "A"
+              :code "[10 15 26]"}
+             {:label "B"
+              :code "(map inc A)"}
+             {:label "C"
+              :code "(reduce + B)"}
+             {:label "D"
+              :code "(count A)"}
+             {:label "E"
+              :code "(/ C D)"}]}))
 
 (defn insert-step-before! [i]
   (swap! state update :steps insert-in-vector i {:code "(fn [i] i)"}))
@@ -32,41 +36,91 @@
 (defn remove-step! [i]
   (swap! state update :steps remove-from-vector i))
 
-(defn calculate-results! []
-   (try
-     (vec (reductions (fn [memo f]
-                       (f memo))
-              (sci/eval-string (:initial-input @state))
-              (map (fn [step]
-                     (sci/eval-string (:code step)))
-                   (:steps @state))))
-     (catch js/Error e
-       [])))
+(defn edit-step-code! [label code]
+  (swap! state update :steps (fn [steps]
+                               (map (fn [step]
+                                     (if (= (:label step)
+                                            label)
+                                      (assoc step :code code)
+                                      step))
+                                    steps))))
 
+#_(defn calculate-results! []
+     (try
+       (vec (reductions (fn [memo f]
+                         (f memo))
+                (sci/eval-string (:initial-input @state))
+                (map (fn [step]
+                       (sci/eval-string (:code step)))
+                     (:steps @state))))
+       (catch js/Error e
+         [])))
+
+
+(defn calculate-results! [steps]
+  (loop [context {}
+         remaining-steps steps]
+   (let [{:keys [label code]} (first remaining-steps)
+         result (try
+                  (sci/eval-string code {:namespaces {'user context}})
+                  (catch js/Error e
+                    e))]
+    (println (type result))
+    (cond
+      (= (type result) ExceptionInfo)
+      (assoc context (symbol label) result)
+      (seq remaining-steps)
+      (recur (assoc context (symbol label) result)
+             (rest remaining-steps))
+      :done
+      context))))
+
+
+#_(let [steps [{:label "A"
+                :code "[10 15 26]"}
+               {:label "B"
+                :code "(map inc A)"}
+               {:label "C"
+                :code "(reduce + B)"}
+               {:label "D"
+                :code "(count A)"}
+               {:label "E"
+                :code "(/ C D)"}]]
+    (calculate-results! steps))
+
+
+
+#_(sci/eval-string "(+ A 3)" {:namespaces {'user {'A 7}}})
 #_(calculate-results!)
 
 (defn app-view []
-  (let [results (calculate-results!)]
+  (let [results (calculate-results! (@state :steps))]
    [:div
-    [:div.initial-value
-     [:textarea {:value (:initial-input @state)
-                 :on-change change-initial-input!}]]
     [:div
      [:button {:on-click (fn [_] (insert-step-before! 0))} "+"]]
-    (for [[index step] (map-indexed vector (:steps @state))]
-      ^{:key index}
+    (for [{:keys [label code]} (:steps @state)
+          :let [result (get results (symbol label) ::NO-RESULT)]]
+      ^{:key label}
       [:<>
        [:div.step
-        [:textarea {:value (:code step)
+        [:textarea {:value code
                     :on-change (fn [e]
-                                (swap! state assoc-in [:steps index :code] (.. e -target -value)))}]
-
-        [:span {} (pr-str (get results (inc index)))]
-        [:button {:on-click (fn [_] (remove-step! index))} "x"]]
+                                 (edit-step-code! label (.. e -target -value)))}]
+        [:span label "=>"]
+        [:span {}
+          (cond
+           (= (type result) ExceptionInfo)
+           (.-message result)
+           (= result ::NO-RESULT)
+           ""
+           :else
+           (pr-str result))]
+        [:button {:on-click (fn [_] #_(remove-step! index))} "x"]]
        [:div
-        [:button {:on-click (fn [_] (insert-step-before! (inc index)))} "+"]]])]))
+        [:button {:on-click (fn [_] #_(insert-step-before! (inc index)))} "+"]]])]))
 
 
-;; deleting
-;; moving
-;; ineserting in middle
+;; temporarily disabling step
+;; moving a step
+;; code autoformatting
+;; uploading a csv
