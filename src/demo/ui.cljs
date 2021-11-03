1174f484e7869a21a3405340975848074d1b318c
Rafal Dittwald <rafal.dittwald@gmail.com>
2021-11-03 00:03:05 +0000

    Add ability to insert step between existing steps

--- src/demo/ui.cljs
+++ src/demo/ui.cljs
@@ -9,6 +9,14 @@
 
 #_(remove-from-vector [:a :b :c :d] 2)
 
+(defn insert-in-vector [vector i value]
+  (vec (concat (subvec vector 0 i)
+               [value]
+               (subvec vector i))))
+
+
+#_(insert-in-vector [:a :b :c] 0 :new)
+
 (defonce state
   (r/atom
     {:initial-input "{:x 1\n:y 2}"
@@ -18,8 +26,8 @@
   (swap! state assoc :initial-input
     (.. e -target -value)))
 
-(defn add-new-step! []
-  (swap! state update :steps conj {:code "(fn [i] i)"}))
+(defn insert-step-before! [i]
+  (swap! state update :steps insert-in-vector i {:code "(fn [i] i)"}))
 
 (defn remove-step! [i]
   (swap! state update :steps remove-from-vector i))
@@ -40,17 +48,23 @@
 (defn app-view []
   (let [results (calculate-results!)]
    [:div
-    [:textarea {:value (:initial-input @state)
-                :on-change change-initial-input!}]
+    [:div.initial-value
+     [:textarea {:value (:initial-input @state)
+                 :on-change change-initial-input!}]]
+    [:div
+     [:button {:on-click (fn [_] (insert-step-before! 0))} "+"]]
     (for [[index step] (map-indexed vector (:steps @state))]
-      [:div
-       [:button {:on-click (fn [_] (remove-step! index))} "x"]
-       [:textarea {:value (:code step)
-                   :on-change (fn [e]
-                               (swap! state assoc-in [:steps index :code] (.. e -target -value)))}]
+      ^{:key index}
+      [:<>
+       [:div.step
+        [:textarea {:value (:code step)
+                    :on-change (fn [e]
+                                (swap! state assoc-in [:steps index :code] (.. e -target -value)))}]
 
-       [:span {} (pr-str (get results (inc index)))]])
-    [:button {:on-click add-new-step!} "+"]]))
+        [:span {} (pr-str (get results (inc index)))]
+        [:button {:on-click (fn [_] (remove-step! index))} "x"]]
+       [:div
+        [:button {:on-click (fn [_] (insert-step-before! (inc index)))} "+"]]])]))
 
 
 ;; deleting
